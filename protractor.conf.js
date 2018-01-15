b8404568617b820d80d3c1b7d4c1e06b7b9170d9
bhargavgunreddy <gbhargavreddy@gmail.com>
2018-01-15 11:12:30 -0500

    using paramos for heroic deploy

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://'+process.env.IP+':'+process.env.PORT+'/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
